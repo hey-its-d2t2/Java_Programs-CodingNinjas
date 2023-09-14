3d03a05c5c88514e74b602c505866ba961c438a3
Deepak Singh <63626210+hey-its-d2t2@users.noreply.github.com>
2023-09-14 11:02:08 +0530

    Update Factorial_of_a_number.java

--- Factorial_of_a_number.java
+++ Factorial_of_a_number.java
@@ -4,24 +4,63 @@ Factorial of n is:
 n! = n * (n-1) * (n-2) * (n-3)....* 1
 Output the factorial of 'n'. If it does not exist, output 'Error'.
 */
+
 import java.util.Scanner;
 
-class Solution {
-	
-	public static void main(String args[]) {
-		
-		// Write code here
-		Scanner sc =new Scanner(System.in);
-		int num =sc.nextInt();
-		long fact =1;
+ 
 
-		for(int i=1;i<=num;i++)
-		{
-			if(num==0 || num==1)
-				break;
-			fact =fact*i;
-		}
-		System.out.print(fact);
-		
-	}
+class Solution {
+
+    
+
+    public static void main(String args[]) {
+
+        
+
+        // Write code here
+
+        
+
+        // Adding Scanner Function 
+
+ 
+
+        Scanner input = new Scanner(System.in);
+
+ 
+
+        int input_number = input.nextInt();
+
+ 
+
+        // Adding factorial function
+
+ 
+
+        if(input_number < 0){
+
+            System.out.println("Error");
+
+        }else{
+
+            long factorial = 1;
+
+            for( int i = 1; i <= input_number; i++){
+
+                factorial *= i;
+
+            }
+
+ 
+
+            System.out.println(factorial);
+
+        }
+
+        
+
+    }
+
 }
+
+ 
